836d3e02c3897dc8fdc6d5f89e849c6bdc3a4c02
Anuj Mishra <anujmishra4131@gmail.com>
2025-11-15 01:23:42 +0530

    Clean main branch for full project setup

--- src/main/java/com/movierecommendationsystem/movie_recommendation_system/repository/MovieRepository.java
+++
@@ -1,9 +0,0 @@
-package com.movierecommendationsystem.movie_recommendation_system.repository;
-import com.movierecommendationsystem.movie_recommendation_system.model.Movie;
-import org.springframework.data.jpa.repository.JpaRepository;
-import java.util.List;
-
-public interface MovieRepository extends JpaRepository<Movie, Long> {
-    List<Movie> findByGenre(String genre);
-}
-
--- src/main/resources/import.sql
+++
@@ -1,4 +0,0 @@
-INSERT INTO movie (id, title, genre, rating) VALUES (1, 'Inception', 'Sci-Fi', 9.0);
-INSERT INTO movie (id, title, genre, rating) VALUES (2, 'The Dark Knight', 'Action', 9.1);
-INSERT INTO movie (id, title, genre, rating) VALUES (3, 'Interstellar', 'Sci-Fi', 8.6);
-INSERT INTO movie (id, title, genre, rating) VALUES (4, 'Avengers: Endgame', 'Action', 8.4);
